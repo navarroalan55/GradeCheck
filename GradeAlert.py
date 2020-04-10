2e9e1e3231d924119c0401f52b0f66644116d32c
alan <navarroalan55@gmail.com>
2020-04-09 20:07:16 -0400

    GUI is complete, log in works, all data is collected from website but not displayable. Clear button needs to reset the grade search.

--- GradeAlert.py
+++ GradeAlert.py
@@ -1,117 +1,141 @@
-import selenium
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import tkinter as tk
+from selenium import webdriver
+from tkinter import *
+from tkinter import messagebox
 from tkinter import ttk
-import os
-import requests
-from io import BytesIO
+import selenium
+import time
 
-global actual_grades
-# makes it go headless
-#options = Options()
-#options.headless = True
+options = Options()
+options.headless = True
 
-# Headless
-#driver = webdriver.Chrome(chrome_options=options)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('/home/alan/Documents/Python/GradeCheck/chromedriver', options = options)
+#add info to second tab, update clear button
+
+class App(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.geometry("450x365")
+        self.resizable(False, False)
+        self.title("Grade Finder")
+        self.notebook = ttk.Notebook()
+        self.notebook.pack(fill="both", expand=1)
+        self.add_tabs()
 
 
-def fetch():
-    driver.get('https://www.fridayparentportal.com/northbergen')
+    def add_tabs(self):
+        tab = Log(self.notebook)
+        tab2 = Grade(self.notebook)
+        self.notebook.add(tab, text="Log in")
+        self.notebook.add(tab2, text="Grades")
 
-    # gets the elements from the web page to log in
-    portalcode_box = driver.find_element_by_name('portalcode')
-    username_box = driver.find_element_by_name('username')
-    password_box = driver.find_element_by_name('password')
 
-    # sends information to web page to log in.
+class Log(Frame):
+    def __init__(self, name, *args, **kwargs):
+        def login():
+            driver.get('https://www.fridayparentportal.com/northbergen')
+            time.sleep(5)
+            portalcode_box = driver.find_element_by_name('portalcode')
+            username_box = driver.find_element_by_name('username')
+            password_box = driver.find_element_by_name('password')
 
-    portalcode_box.send_keys(portalcode.get())
-    username_box.send_keys(username.get())
-    password_box.send_keys(password.get())
+            portalcode_box.send_keys(t_code.get())
+            username_box.send_keys(t_user.get())
+            password_box.send_keys(t_pass.get())
 
-    # presses the log in button
-    login_button = driver.find_element_by_css_selector('button.btn')
-    login_button.click()
+            login_button = driver.find_element_by_css_selector('button.btn')
+            login_button.click()
 
-    try:
-        # opens up to the gradebook
-        gradebook = driver.find_element_by_link_text('Gradebook')
-        gradebook.click()
+            try:
+                gradebook = driver.find_element_by_link_text('Gradebook')
+                gradebook.click()
 
-        # saves student picture
-        student_pic = driver.find_element_by_css_selector('img.img').get_attribute('src')
-        print(student_pic)
-        #s = urllib.request.urlretrieve(student_pic, "s.jpeg")
+                student_pic = driver.find_element_by_css_selector('img.img').get_attribute('src')
+                print(student_pic)
 
-        #
-        #response = requests.get(student_pic)
-        #picR = response.content
-        #image = tk.PhotoImage(data = picR)
-        #label = tk.Label(image=image)
-        #label.pack(page2)
-
-        # finds the name of the courses/ teacher/ grade
-
-        list_course = driver.find_elements_by_css_selector('td.finedetail')
-
-        for n in list_course:
-            grades_listed = [n.text]
-
-            grades = ttk.Label(page2, text=n.text)
-            grades.pack()
-
-    except selenium.common.exceptions.NoSuchElementException:
-        print("Mhmm did you type your login correctly? Try again.")
-
-HEIGHT = 500
-WIDTH = 600
-
-# make window
-root = tk.Tk()
-root.title("Grade Finder")
-
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
-
-frame = tk.Frame(root)
-frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-
-# makes tabs
-NB = ttk.Notebook(frame)
-NB.place(relx=0, rely=0, relwidth=1, relheight=1)
-page1 = ttk.Frame(NB)
-page2 = ttk.Frame(NB)
-NB.add(page1, text='Login')
-NB.add(page2, text='Grades')
+                list_course = driver.find_elements_by_css_selector('td.finedetail')
+                lists = [n.text for n in list_course]
+                new_list = [n for n in lists if n != ""]
+                s_class = [n.split("\n")[0] for n in new_list[::3]]
+                s_grades = [n.split("/")[0] for n in new_list[1::3]]
+                s_teacher = new_list[2::3]
+                print(s_class,s_grades, s_teacher)
+                Grade.__init__(self).tree.insert()
 
 
 
-# page 1
-# portalcode
-portalcode = ttk.Entry(page1)
-portalcode.pack()
-
-# username
-username = ttk.Entry(page1)
-username.pack()
-
-#password
-password = ttk.Entry(page1, show="*")
-password.pack()
-
-NB.pack(expand=1, fill="both")
-
-# page 2
-refresh = ttk.Button(page2, text="REFRESH", command=fetch)
-
-refresh.pack()
+            except selenium.common.exceptions.NoSuchElementException:
+                messagebox.showinfo("Error", "Mhmm did you type your login correctly? Try again.")
 
 
 
-root.mainloop()
+        def clear():
+            t_code.delete(0, 'end')
+            t_user.delete(0, 'end')
+            t_pass.delete(0, 'end')
+
+        Frame.__init__(self, *args, **kwargs)
+        self.name = name
+        self.f_log = Frame(self)
+        f_code = Frame(self.f_log)
+        l_code = Label(f_code, text=" Portal Code: ")
+        t_code = Entry(f_code)
+        t_code.focus_set()
+
+        f_user = Frame(self.f_log, bg="#2863A4")
+        l_user = Label(f_user, text="Username:    ")
+        t_user = Entry(f_user)
+
+        f_pass = Frame(self.f_log)
+        l_pass = Label(f_pass, text="Password:      ")
+        t_pass = Entry(f_pass, show="*")
+
+        f_b = Frame(self.f_log)
+        b_log = Button(f_b, text="LOG IN", command= login)
+        b_c_log = Button(f_b, text="CLEAR ", command= clear)
+
+        self.f_log.pack(fill="both", expand=1, padx=50, pady=100)
+
+        f_code.pack(fill="both", expand=1, pady=5)
+        l_code.pack(fill="y", side="left")
+        t_code.pack(fill="both", side="left", expand=1)
+
+        f_user.pack(fill="both", expand=1)
+        l_user.pack(fill="y", side="left")
+        t_user.pack(fill="both", side="left", expand=1)
+
+        f_pass.pack(fill="both", expand=1)
+        l_pass.pack(fill="y", side="left")
+        t_pass.pack(fill="both", side="left", expand=1, pady=5)
+
+        f_b.pack()
+        b_log.pack(side="left")
+        b_c_log.pack(side="left",padx =5)
 
 
+class Grade(Frame):
+
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+        self.f_grade = Frame(self)
+        f_user = Frame(self.f_grade)
+
+        self.f_grades = Frame(self.f_grade)
+        self.tree = ttk.Treeview(self.f_grades)
+        self.tree["columns"] = ("one", "two")
+        self.tree.column('#0', width=150)
+        self.tree.column("one", width=100)
+        self.tree.column("two", width=30)
+        self.tree.heading("#0", text="Class")
+        self.tree.heading("one", text="Teacher")
+        self.tree.heading("two", text="Grade")
+        self.tree.pack(fill="both", expand=1)
+
+        self.f_grade.pack(fill="both", expand=1)
+        f_user.place(height=100, width=450)
+
+        self.f_grades.place(y=100, height=350, width=450)
 
 
+my_app = App()
+my_app.mainloop()
